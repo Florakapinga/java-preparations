6e95acc86da9f498d8b9a1496830942462e713f1
kapinga <erickfloriana@gmail.com>
2025-06-02 17:23:31 +0300

    first condition

--- Average.java
+++ Average.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 public class Average {
+  int marks;
     public void main(String[]args){
       Scanner x =  new Scanner(System.in);
       System.out.println("Enter the average");
       int n = x.nextInt();
+      
+      if (n<0|) {
+        
+      }
     }
 }

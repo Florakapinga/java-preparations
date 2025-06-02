0fb171b831cdf5af89b07e5db1a0788c7beed556
kapinga <erickfloriana@gmail.com>
2025-06-02 17:19:00 +0300

    another commit

--- Average.java
+++ Average.java
@@ -3,5 +3,6 @@ public class Average {
     public void main(String[]args){
       Scanner x =  new Scanner(System.in);
       System.out.println("Enter the average");
+      int n = x.nextInt();
     }
 }
